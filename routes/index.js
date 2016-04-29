909f17ceb3cbffe28a7b55a71e414f62c23f1627
Preston Watson <preston.watson89@gmail.com>
2016-04-29 12:37:11 -0500

    adding routes and additona views edits

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+/* login authentication function */
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.redirect('/');
+//   }
+// }
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('front_page');
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+})
+
+/* GET profile page. */
+router.get('/profile', isLoggedIn, function(req, res, next) {
+  res.render('profile');
+})
+
+/* GET search page. */
+router.get('/search', function(req, res, next) {
+  res.render('search');
+})
 
 module.exports = router;
