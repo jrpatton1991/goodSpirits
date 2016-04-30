7e21a4cf0de6528f1276fd1ee7f1660069b4a1e0
Christopher Porter <snooper90@gmail.com>
2016-04-30 08:12:42 -0500

    basic functionality for search in place

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,45 @@
+//global vars
+var beerName = [];
+
+// on dom load
 $(document).ready(function() {
+  //ajax call to get the list of beers
+  getBeers();
+  // keypress event listener for search
+  $('#searchForBeer').keypress(function(e){
+    console.log('good')
+    updateBeerList();
+  });
+
+});
+
+function updateBeerList(){
+  string = $('#searchForBeer').val();
+  var newRegexpSearch = new RegExp(string, 'gi')
+  //searchedBeers = beerName;
+  var searchedBeers = [];
+  for (var i = 0, j = 0; i < beerName.length; i++) {
+    if (beerName[i].match(new RegExp(string, "gi"))){
+      searchedBeers[j] = beerName[i]
+      j++;
+    }
+  }
+  console.log(searchedBeers)
+
+  $('ul').html(null)
+  listArr(searchedBeers)
+}
+function listArr(list){
+  console.log(list[0]);
+  for(i = 0; i < list.length; i++){
+    $('ul').append('<li>' + list[i] + '</li>');
+  };
+}
+
+
+
+
+function getBeers(){
   $.ajax({
     url: '/drinks/beers',
     method:'get',
@@ -6,12 +47,11 @@ $(document).ready(function() {
   })
   .done(function(data, textStatus){
     for( var i = 0; i < data.length; i++){
-      $('#beerList').append('<li>' + data[i].name + '</li>')
+      beerName.push(data[i].name);
     }
 
   })
   .fail(function(err, textStatus){
     console.log(err);
   })
-
-});
+}
