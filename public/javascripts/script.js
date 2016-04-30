71f00a9e3bec7548de5e46ea5a32d2b9be2e821e
Christopher Porter <snooper90@gmail.com>
2016-04-29 22:49:32 -0500

    api is in working order to list out the names of all the beers

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+  $.ajax({
+    url: '/drinks/beers',
+    method:'get',
+    dataType:'json'
+  })
+  .done(function(data, textStatus){
+    for( var i = 0; i < data.length; i++){
+      $('#beerList').append('<li>' + data[i].name + '</li>')
+    }
 
+  })
+  .fail(function(err, textStatus){
+    console.log(err);
+  })
 
 });
--- routes/drinks.js
+++ routes/drinks.js
@@ -8,14 +8,15 @@ var apiKey = process.env.LCBO_KEY;
 router.get('/beers', function(req,res,next){
   var beers = []
   for( var i = 1; i <= 11 ; i ++){
+
     request.get( 'http://www.lcboapi.com/products?per_page=100&page='+ i +'&q=beer&access_key=' + apiKey,
      function(err, data){
-       var beerList = JSON.parse(data.body);
-       beers = beers.concat(beers, beerList);
-       console.log(beerList.pager.is_final_page)
 
-       if(beerList.pager.is_final_page){
-         console.log(beerList.pager.is_final_page)
+       var beerList = JSON.parse(data.body);
+       Array.prototype.push.apply(beers, beerList.result);
+       console.log(beers.length);
+       if(beers.length >= 1030){
+         console.log(beers.length)
          res.json(beers);
        };
     });
