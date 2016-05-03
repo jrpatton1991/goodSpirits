4bb774569f5105dda52e76f1891be737ad140fda
Jacob Patton <jrobertpatton@gmail.com>
2016-05-03 14:32:02 -0500

    setup ajax get request

--- public/javascripts/beersLiked.js
+++ public/javascripts/beersLiked.js
@@ -1,1 +1,50 @@
+$(document).ready(function(){
+ getBeer();
+ getLikes();
+
+
+ }
+
+
+//function to get Beer data from API
+
+function getBeer() {
+  $.ajax({
+    url: '/beer/:id',
+    type: 'GET',
+    dataType: 'JSON',
+    data: {param1: ''},
+  })
+  .done(function() {
+    console.log("success");
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+}
+
+
+
+function getLikes() {
+//Likes from our own DB
+  $.ajax({
+    url: 'user/',
+    type: 'GET',
+    dataType: 'JSON',
+    data: {id: userId , beerId: beerId },
+  })
+  .done(function() {
+    console.log("success");
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+  }
 
