e5f3ca6e2c2ed9b5836e8c6f1b2a1a2f75b7e2c2
Preston Watson <preston.watson89@gmail.com>
2016-05-03 11:21:09 -0500

    pulling likes

--- routes/likes.js
+++ routes/likes.js
@@ -8,13 +8,14 @@ var mongoose = require('mongoose');
 /* Post Likes */
 router.post('/', function(req, res, next) {
   var userId = req.user._id;
-  var beerId = req.params.id;
-  var likes = new Likes({ userId: userId, beerId: beerId });
+  var beerId = req.body.id;
+  var username = req.user.username;
+  var likes = new Likes({ userId: userId, beerId: beerId, username: username });
 
   Likes.find({ "userId": userId, "beerId": beerId }, function(error, data) {
-    console.log(data);
+    // console.log(data);
     console.log(error);
-    if (data.length == 0) {
+    if (data.length === 0) {
      likes.save(likes, function(error) {
        if (error) {
         res.send(error);
@@ -26,6 +27,15 @@ router.post('/', function(req, res, next) {
        console.log('NO!');
     }
   })
+});
+
+router.get('/user', function(req, res, next) {
+  var userId = req.user._id;
+
+  Likes.find({ "userId": userId }, function(error, data) {
+
+  res.send(data)
+})
 });
 
 module.exports = router;
