148ea4a7aff91782af098224ba4e0e827de59d8d
Preston Watson <preston.watson89@gmail.com>
2016-05-04 10:46:48 -0500

    api is back up

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
-$.ajax({
-  url:'/likes/user',
+getLikes();
+
+});
+
+
+function getLikes(){
+  $.ajax({
+  url:'/api/me',
   method:'GET',
   dataType:'JSON'
-})
-.done(function(data, textStatus){
-    beerList = data;
-    console.log(beerList);
+  })
+  .done(function(data, textStatus){
+    getBeers(data)
   })
   .fail(function(data, textStatus){
     console.log("ERROR getting likes. status: " + textStatus);
   });
-});
+}
+
+function getBeers(arrLikes){
+  var beerList = [];
+
+  $.ajax({
+  url:'/api/beers/' + arrLikes[0].beerId,
+  method:'GET',
+  dataType:'JSON'
+  })
+  .done(function(data, textStatus){
+    console.log(data);
+  })
+  .fail(function(data, textStatus){
+    console.log(data)
+    console.log("ERROR getting beers. status: " + textStatus);
+  });
+}
--- routes/drinks.js
+++ routes/drinks.js
@@ -1,35 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var apiKey = process.env.LCBO_KEY;
-/* GET users listing. */
-
-router.get('/likes', function(req,res,next){
-  console.log('the get likes endpoint has been hit');
-});
-
-router.get('/beers', function(req,res,next){
-  var beers = [];
-  for( var i = 1; i <= 11 ; i ++){
-
-    request.get('http://www.lcboapi.com/products?per_page=100&page='+ i +'&q=beer&access_key=' + apiKey,
-     function(err, data){
-       var beerList = JSON.parse(data.body);
-       Array.prototype.push.apply(beers, beerList.result);
-       console.log(beers.length);
-       if(beers.length >= 1022){
-         res.json(beers);
-       }
-    });
-  }
-});
 
 
+router.get('/:id', function(req, res, next) {
+  var beerId = req.params.id;
 
-
-module.exports = router;
-
-
-
+  res.render('beer');
+})
 
 module.exports = router;
